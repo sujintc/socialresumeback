b08440a9a759339336cc6b098f492c01d7278c97
sujintc <tcsujin99@gmail.com>
2024-06-01 11:01:50 +0530

    'se'

--- index.js
+++ index.js
@@ -1,72 +1,73 @@
-// // import express from "express";
-// // import bodyParser from "body-parser";
-// // import mongoose from "mongoose";
-// // import cors from "cors";
-// // import dotenv from "dotenv";
-// // import multer from "multer";
-// // import helmet from "helmet";
-// // import morgan from "morgan";
-// // import path from "path";
-// // import { fileURLToPath } from "url";
-// // import authRoutes from "./routes/auth.js";
-// // import userRoutes from "./routes/users.js";
-// // import postRoutes from "./routes/posts.js";
-// // import { register } from "./controllers/auth.js";
-// // import { createPost } from "./controllers/posts.js";
-// // import { verifyToken } from "./middleware/auth.js";
-// // import User from "./models/User.js";
-// // import Post from "./models/Post.js";
-// // import { users, posts } from "./data/index.js";
 
-// // /* CONFIGURATIONS */
-// // const __filename = fileURLToPath(import.meta.url);
-// // const __dirname = path.dirname(__filename);
-// // dotenv.config();
-// // const app = express();
-// // app.use(express.json());
-// // app.use(helmet());
-// // app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-// // app.use(morgan("common"));
-// // app.use(bodyParser.json({ limit: "30mb", extended: true }));
-// // app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-// // app.use(cors());
-// // app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-// // /* FILE STORAGE */
-// // const storage = multer.diskStorage({
-// //   destination: function (req, file, cb) {
-// //     cb(null, "public/assets");
-// //   },
-// //   filename: function (req, file, cb) {
-// //     cb(null, file.originalname);
-// //   },
-// // });
-// // const upload = multer({ storage });
+// import express from "express";
+// import bodyParser from "body-parser";
+// import mongoose from "mongoose";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import multer from "multer";
+// import helmet from "helmet";
+// import morgan from "morgan";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import authRoutes from "./routes/auth.js";
+// import userRoutes from "./routes/users.js";
+// import postRoutes from "./routes/posts.js";
+// import { register } from "./controllers/auth.js";
+// import { createPost } from "./controllers/posts.js";
+// import { verifyToken } from "./middleware/auth.js";
+// import User from "./models/User.js";
+// import Post from "./models/Post.js";
+// import { users, posts } from "./data/index.js";
 
-// // /* ROUTES WITH FILES */
-// // app.post("/auth/register", upload.single("picture"), register);
-// // app.post("/posts", verifyToken, upload.single("picture"), createPost);
+// /* CONFIGURATIONS */
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// dotenv.config();
+// const app = express();
+// app.use(express.json());
+// app.use(helmet());
+// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+// app.use(morgan("common"));
+// app.use(bodyParser.json({ limit: "30mb", extended: true }));
+// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// app.use(cors());
+// app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-// // /* ROUTES */
-// // app.use("/auth", authRoutes);
-// // app.use("/users", userRoutes);
-// // app.use("/posts", postRoutes);
+// /* FILE STORAGE */
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "public/assets");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+// const upload = multer({ storage });
 
-// // /* MONGOOSE SETUP */
-// // const PORT = process.env.PORT || 6001;
-// // mongoose
-// //   .connect(process.env.MONGO_URL, {
-// //     useNewUrlParser: true,
-// //     useUnifiedTopology: true,
-// //   })
-// //   .then(() => {
-// //     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+// /* ROUTES WITH FILES */
+// app.post("/auth/register", upload.single("picture"), register);
+// app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-// //     /* ADD DATA ONE TIME */
-// //     // User.insertMany(users);
-// //     // Post.insertMany(posts);
-// //   })
-// //   .catch((error) => console.log(`${error} did not connect`));
+// /* ROUTES */
+// app.use("/auth", authRoutes);
+// app.use("/users", userRoutes);
+// app.use("/posts", postRoutes);
+
+// /* MONGOOSE SETUP */
+// const PORT = process.env.PORT || 6001;
+// mongoose
+//   .connect(process.env.MONGO_URL)
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+//     /* ADD DATA ONE TIME */
+//     // User.insertMany(users);
+//     // Post.insertMany(posts);
+//   })
+//   .catch((error) => console.log(`${error} did not connect`));
+
 
 
 import express from "express";
@@ -93,7 +94,9 @@ import { users, posts } from "./data/index.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
+
 const app = express();
+
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -125,15 +128,20 @@ app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
+
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
 
     /* ADD DATA ONE TIME */
     // User.insertMany(users);
     // Post.insertMany(posts);
   })
-  .catch((error) => console.log(`${error} did not connect`));
-
+  .catch((error) => console.error("Failed to connect to MongoDB", error));
